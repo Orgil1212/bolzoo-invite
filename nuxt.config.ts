e8f188328ab976bdc1cb437d191427354426489e
Orgil1212 <orgiloorgil16@gmail.com>
2025-04-17 23:27:08 +0800

    ✅ Fix: Use Node.js 18 for Nuxt on Render

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['@/assets/css/tailwind.css'],
+
   postcss: {
     plugins: {
       '@tailwindcss/postcss': {}, // шинэ plugin
       autoprefixer: {},
     },
   },
+
+  compatibilityDate: '2025-04-17',
 })
